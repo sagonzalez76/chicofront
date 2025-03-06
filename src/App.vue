91137c5e073862e2ef2c8ac057440f6f73a9ecd8
Julian <julianc416@gmail.com>
2025-03-06 17:17:01 -0500

    refactor: clean up App.vue, add custom font, and introduce new icons

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-<h1 class="font-seocndary text-4xl">PAPAPAP</h1>
-      <nav class="font-worksans font-bold">
-        <RouterLink class="bg-primary-100" to="/">Homee</RouterLink> <br>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
