53d0122e68237a5712b0b2917277fab0df0098d8
sagonzalez76 <sagonzalez76@misena.edu.co>
2025-03-07 13:24:41 -0500

    feat: update HeroSection with image carousel and search functionality; refactor IconSearch component

--- src/components/icons/IconSearch.vue
+++ src/components/icons/IconSearch.vue
@@ -1,7 +1,52 @@
+<script lang="ts" setup>
+defineProps({
+  width: {
+    type: Number,
+    default: 24,
+  },
+  height: {
+    type: Number,
+    default: 24,
+  },
+  color: {
+    type: String,
+    default: 'text-gray-800',
+  },
+  strokeWidth: {
+    type: Number,
+    default: 2,
+  },
+  fill: {
+    type: String,
+    default: 'none',
+  },
+  ariaLabel: {
+    type: String,
+    default: 'icon',
+  },
+  role: {
+    type: String,
+    default: 'img',
+  },
+})
+</script>
+
 <template>
-  <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-    height="24" fill="none" viewBox="0 0 24 24">
-    <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
-      d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
+  <svg
+    :width="width"
+    :height="height"
+    :class="color"
+    :aria-label="ariaLabel"
+    :role="role"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    :fill="fill"
+  >
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      :stroke-width="strokeWidth"
+      d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+    />
   </svg>
 </template>
--- src/components/landing/HeroSection.vue
+++ src/components/landing/HeroSection.vue
@@ -1,131 +1,74 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
 import IconSearch from '../icons/IconSearch.vue'
+
+const currentIndex = ref(0)
+const currentOption = ref(0)
+const images = [
+  'public/images/Stadium1.jpg',
+  'public/images/Stadium2.jpg',
+  'public/images/Stadium3.jpg',
+  'public/images/Stadium4.jpg',
+]
+
+const nextSlide = () => {
+  currentIndex.value = (currentIndex.value + 1) % images.length
+}
+
+onMounted(() => {
+  setInterval(nextSlide, 10000)
+})
 </script>
+
 <template>
-  <div class="hero-section">
-    <div class="carousel">
-      <div class="carousel-inner" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
-        <div class="carousel-item" v-for="(image, index) in images" :key="index">
-          <img :src="image" class="w-full h-screen object-cover filter-dark overlay-red" />
+  <div class="w-full overflow-hidden relative">
+    <div class="flex items-center justify-center relative">
+      <div
+        class="flex transition-transform duration-1000 ease-in-out"
+        :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
+      >
+        <div v-for="(image, index) in images" :key="index" class="min-w-full relative">
+          <img :src="image" class="w-full h-screen object-cover brightness-50" />
+          <div class="absolute inset-0 bg-gradient-to-t from-red-500/45 to-transparent"></div>
         </div>
       </div>
-      <button class="carousel-control prev" @click="prevSlide">‹</button>
-      <button class="carousel-control next" @click="nextSlide">›</button>
     </div>
-    <div class="overlay">
+
+    <div
+      class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-center z-10 w-11/12 md:w-2/5"
+    >
       <h1 class="text-white text-4xl font-bold mb-4">Bienvenido a Nuestro Sitio</h1>
-      <h1 class="text-white text-xl mb-4">Fields, teams and utilities in just few Clicks</h1>
-      <div class="flex p-1 rounded-4xl items-center text-sm bg-gray-300 ">
-        <IconSearch class="w-4 h-4 text-gray-500 " />
-        <input type="text" placeholder="Buscar..." class="p-2 rounded w-64 outline-0 "/>
-        <span class="text-white ">|</span>
-        <select name="" id="" class="p-2 rounded ml-2 w-52 justify-end ">
-          <option value="1">Futbol</option>
-          <option value="2">Basket</option>
-          <option value="3">Volley</option>
-        </select>
-        <IconSearch class="w-10 h-10 text-gray-500 bg-primary rounded-4xl p-2 m-1" />
+      <h2 class="text-white text-xl mb-4">Fields, teams and utilities in just few Clicks</h2>
+
+      <div class="flex rounded-3xl items-center text-sm bg-gray-300">
+        <div class="flex py-2 items-center w-1/2 border-r-2 border-gray-200 gap-1">
+          <div class="flex items-center p-2 m-1 rounded-full">
+            <IconSearch :width="24" :height="24" fill="transparent" color="text-letters" />
+          </div>
+          <input
+            type="text"
+            placeholder="Buscar..."
+            class="w-full bg-transparent outline-none placeholder:text-sm sm:placeholder:text-base"
+          />
+        </div>
+
+        <div class="flex items-center w-1/2 gap-3 p-2">
+          <select
+            class="p-1 rounded ml-2 w-full text-center bg-transparent outline-none"
+            :class="{ 'text-letters': currentOption == 0 }"
+            v-model="currentOption"
+          >
+            <option value="0" selected disabled>Seleccionar opción</option>
+            <option value="1">Fútbol</option>
+            <option value="2">Básquet</option>
+            <option value="3">Vóley</option>
+          </select>
+          <div class="flex items-center p-2 rounded-full bg-primary text-white">
+            <IconSearch :width="24" :height="24" fill="transparent" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
-<script lang="ts">
-export default {
-  data() {
-    return {
-      currentIndex: 0,
-      images: [
-        'public/images/Stadium1.jpg',
-        'public/images/Stadium2.jpg',
-        'public/images/Stadium3.jpg',
-      ],
-    }
-  },
-  methods: {
-    nextSlide() {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length
-    },
-    prevSlide() {
-      this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length
-    },
-  },
-  mounted() {
-    setInterval(this.nextSlide, 5000)
-  },
-}
-</script>
-
-<style scoped>
-.hero-section {
-  width: 100%;
-  overflow: hidden;
-  position: relative;
-}
-
-.carousel {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-}
-
-.carousel-inner {
-  display: flex;
-  transition: transform 2s ease-in-out;
-}
-
-.carousel-item {
-  min-width: 100%;
-  position: relative;
-}
-
-.carousel-item img {
-  width: 100%;
-  height: 100vh;
-  object-fit: cover;
-}
-
-.filter-dark {
-  filter: brightness(50%);
-}
-
-.overlay-red::after {
-  content: '';
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 50%;
-  background: linear-gradient(to top, rgba(255, 0, 0, 0.5), transparent);
-}
-
-.overlay {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  z-index: 10;
-}
-
-.carousel-control {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  background: rgba(0, 0, 0, 0.5);
-  color: white;
-  border: none;
-  padding: 10px;
-  cursor: pointer;
-  z-index: 10;
-}
-
-.carousel-control.prev {
-  left: 10px;
-}
-
-.carousel-control.next {
-  right: 10px;
-}
-</style>
+<style scoped></style>
