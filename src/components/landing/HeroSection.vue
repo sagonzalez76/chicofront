6cd7dff9979d1b6e123a900c77435c133429df31
Julian <julianc416@gmail.com>
2025-03-07 12:23:07 -0500

    feat: agregar búsqueda y mejorar diseño en HeroSection

--- src/components/landing/HeroSection.vue
+++ src/components/landing/HeroSection.vue
@@ -1,9 +1,12 @@
+<script setup lang="ts">
+import IconSearch from '../icons/IconSearch.vue'
+</script>
 <template>
   <div class="hero-section">
     <div class="carousel">
       <div class="carousel-inner" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
         <div class="carousel-item" v-for="(image, index) in images" :key="index">
-          <img :src="image" class="w-full h-screen object-cover filter-dark" />
+          <img :src="image" class="w-full h-screen object-cover filter-dark overlay-red" />
         </div>
       </div>
       <button class="carousel-control prev" @click="prevSlide">‹</button>
@@ -11,7 +14,18 @@
     </div>
     <div class="overlay">
       <h1 class="text-white text-4xl font-bold mb-4">Bienvenido a Nuestro Sitio</h1>
-      <input type="text" placeholder="Buscar..." class="p-2 rounded" />
+      <h1 class="text-white text-xl mb-4">Fields, teams and utilities in just few Clicks</h1>
+      <div class="flex p-1 rounded-4xl items-center text-sm bg-gray-300 ">
+        <IconSearch class="w-4 h-4 text-gray-500 " />
+        <input type="text" placeholder="Buscar..." class="p-2 rounded w-64 outline-0 "/>
+        <span class="text-white ">|</span>
+        <select name="" id="" class="p-2 rounded ml-2 w-52 justify-end ">
+          <option value="1">Futbol</option>
+          <option value="2">Basket</option>
+          <option value="3">Volley</option>
+        </select>
+        <IconSearch class="w-10 h-10 text-gray-500 bg-primary rounded-4xl p-2 m-1" />
+      </div>
     </div>
   </div>
 </template>
@@ -22,24 +36,24 @@ export default {
     return {
       currentIndex: 0,
       images: [
-        'https://assets.goal.com/images/v3/blt0bea61fc82528a87/dbbd6d07d4b7a8ed98fa0e0c37b3516c9aef605b.jpg',
-        'https://www.directvsports.com/__export/1707399853068/sites/dsports/img/2024/02/08/untitled_design_-93-.png_1937756784.png',
-        'https://i.imgur.com/geKvCH9.jpg'
-      ]
-    };
+        'public/images/Stadium1.jpg',
+        'public/images/Stadium2.jpg',
+        'public/images/Stadium3.jpg',
+      ],
+    }
   },
   methods: {
     nextSlide() {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      this.currentIndex = (this.currentIndex + 1) % this.images.length
     },
     prevSlide() {
-      this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
-    }
+      this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length
+    },
   },
   mounted() {
-    setInterval(this.nextSlide, 5000);
-  }
-};
+    setInterval(this.nextSlide, 5000)
+  },
+}
 </script>
 
 <style scoped>
@@ -74,6 +88,16 @@ export default {
 
 .filter-dark {
   filter: brightness(50%);
+}
+
+.overlay-red::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50%;
+  background: linear-gradient(to top, rgba(255, 0, 0, 0.5), transparent);
 }
 
 .overlay {
