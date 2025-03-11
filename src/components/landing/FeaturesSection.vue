1a94d34c5826c54a07511d046197a23a1f147b52
Julian <julianc416@gmail.com>
2025-03-11 17:45:17 -0500

    feat: agregar componente BaseCard y mejorar la sección de características con soporte para tarjetas de propiedades

--- src/components/landing/FeaturesSection.vue
+++ src/components/landing/FeaturesSection.vue
@@ -2,22 +2,59 @@
 import { ref } from 'vue';
 import LineButtons from '@/components/shared/LineButtons.vue';
 import LineBar from '@/components/shared/LineBar.vue';
+import BaseCard from '../shared/Base/BaseCard.vue';
 
 interface Button {
-  label: string;
-  count?: number;
-  isActive?: boolean;
+  label: string
+  classButtonCustom: string
+  iconClassCustom: string
+  classLabel?: string
+  icon?: string
+  onClick: () => void
 }
 
-const buttons = ref<Button[]>([
-  { label: 'All Items', count: 5125, isActive: true },
-  { label: 'Residential Spaces', count: 64 },
-  { label: 'Sports Venues' },
-  { label: 'Meeting Spaces' },
-  { label: 'Vans & Buses' },
-  { label: 'Cars & SUVs' },
-  { label: 'Lorries & Industrial Vehicles' }
-]);
+const buttons: Button[] = [
+  {
+    classButtonCustom: '',
+    iconClassCustom: 'w-8 h-8 bg-red-100 mr-4 rounded-full text-primary p-1',
+    label: 'Holiday Rentals',
+    onClick: () => {
+      console.log('Fields')
+    },
+  },
+  {
+    classButtonCustom: '',
+    iconClassCustom: 'w-8 h-8 bg-green-100 mr-4 rounded-full text-green-600 p-1',
+    label: 'Residential Spaces',
+    onClick: () => {
+      console.log('Mvp')
+    },
+  },
+  {
+    classButtonCustom: '',
+    iconClassCustom: 'w-8 h-8 bg-purple-100 mr-4 rounded-full text-purple-600 p-1',
+    label: 'Event Spaces',
+    onClick: () => {
+      console.log('Teams')
+    },
+  },
+  {
+    classButtonCustom: '',
+    iconClassCustom: 'w-8 h-8 bg-blue-100 mr-4 rounded-full text-sky-600 p-1',
+    label: 'Commercial Properities',
+    onClick: () => {
+      console.log('Teams')
+    },
+  },
+  {
+    classButtonCustom: '',
+    iconClassCustom: 'w-8 h-8 bg-orange-100 mr-4 rounded-full text-orange p-1',
+    label: 'More',
+    onClick: () => {
+      console.log('Teams')
+    },
+  },
+]
 
 const containerRef = ref<HTMLElement | null>(null);
 
@@ -32,6 +69,72 @@ const scrollRight = () => {
     containerRef.value.scrollBy({ left: 200, behavior: 'smooth' });
   }
 };
+
+const cards = ref([
+  {
+    images: ['../images/Stadium1.jpg','../images/Stadium2.jpg'],
+    price: 85000,
+    currency: 'COL',
+    title: 'Luxury villa in Rego Park',
+    city: 'Popayán',
+    time: 1,
+    players: 11,
+  },
+  {
+    images: ['../images/Stadium1.jpg','../images/Stadium2.jpg'],
+    price: 75000,
+    currency: 'COL',
+    title: 'Modern apartment in Cali',
+    city: 'Cali',
+    time: 2,
+    players: 8,
+  },
+  {
+    images: ['../images/Stadium1.jpg','../images/Stadium2.jpg'],
+    price: 65000,
+    currency: 'COL',
+    title: 'Cozy house in Medellín',
+    city: 'Medellín',
+    time: 3,
+    players: 5,
+  },
+  {
+    images: ['../images/Stadium1.jpg','../images/Stadium2.jpg'],
+    price: 65000,
+    currency: 'COL',
+    title: 'Cozy house in Medellín',
+    city: 'Medellín',
+    time: 3,
+    players: 5,
+  },
+  {
+    images: ['../images/Stadium1.jpg','../images/Stadium2.jpg'],
+    price: 65000,
+    currency: 'COL',
+    title: 'Cozy house in Medellín',
+    city: 'Medellín',
+    time: 3,
+    players: 5,
+  },
+  {
+    images: ['../images/Stadium1.jpg','../images/Stadium2.jpg'],
+    price: 65000,
+    currency: 'COL',
+    title: 'Cozy house in Medellín',
+    city: 'Medellín',
+    time: 3,
+    players: 5,
+  },
+  {
+    images: ['../images/Stadium1.jpg','../images/Stadium2.jpg'],
+    price: 65000,
+    currency: 'COL',
+    title: 'Cozy house in Medellín',
+    city: 'Medellín',
+    time: 3,
+    players: 5,
+  },
+]);
 </script>
 
 <template>
@@ -49,7 +152,6 @@ const scrollRight = () => {
         ◀
       </button>
       
-      
       <!-- Contenedor scrollable -->
       <div
         ref="containerRef"
@@ -59,7 +161,7 @@ const scrollRight = () => {
         <LineButtons
           :buttons="buttons"
           :classContainer="'flex gap-4 overflow-x-auto scrollbar-hide whitespace-nowrap px-2 py-2'"
-          :classButton="'flex items-center px-4 py-2 rounded-full text-sm font-semibold transition-all bg-gray-200 hover:bg-gray-300'"
+          :classButton="'flex items-center px-4 py-2 rounded-full text-sm font-semibold transition-all bg-gray-200 hover:bg-primary hover:text-white'"
           :classLabel="''"
         />
       </div>
@@ -71,6 +173,20 @@ const scrollRight = () => {
       >
         ▶
       </button>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+      <BaseCard
+        v-for="(card, index) in cards"
+        :key="index"
+        :images="card.images"
+        :price="card.price"
+        :currency="card.currency"
+        :title="card.title"
+        :city="card.city"
+        :time="card.time"
+        :players="card.players"
+      />
     </div>
   </section>
 </template>
--- src/components/shared/Base/BaseCard.vue
+++ src/components/shared/Base/BaseCard.vue
@@ -1,3 +1,74 @@
-<script lang="ts">
+<script lang="ts" setup>
+import { ref } from 'vue'
 
+const props = defineProps({
+  images: {
+    type: Array as () => string[],
+    required: true,
+    default: () => []
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 4100,
+  },
+  currency: {
+    type: String,
+    default: 'LKR',
+  },
+  title: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  city: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  time: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
+  players: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
+})
+
+const currentImageIndex = ref(0)
+
+const nextImage = () => {
+  currentImageIndex.value = (currentImageIndex.value + 1) % props.images.length
+}
+
+const prevImage = () => {
+  currentImageIndex.value = (currentImageIndex.value - 1 + props.images.length) % props.images.length
+}
 </script>
+
+<template>
+  <div class="border rounded-lg shadow max-w-sm bg-white relative">
+    <div class="relative">
+      <img :src="props.images[currentImageIndex]" alt="Property Image" class="w-full h-40 object-cover rounded-lg mb-4" />
+      <button @click="prevImage" class="absolute -left-0 top-1/2 transform -translate-y-1/2  text-black p-1 rounded-full">‹</button>
+      <button @click="nextImage" class="absolute -right-0 top-1/2 transform -translate-y-1/2  text-black p-1 rounded-full">›</button>
+    </div>
+    <div class="flex justify-end">
+      <span class="bg-green-500 text-white text-sm px-3 py-1 rounded-full">For Rent</span>
+    </div>
+    <div class="mt-4">
+      <p class="text-red-500 text-lg font-semibold">{{ props.price }} {{ props.currency }} /Month</p>
+      <h2 class="text-xl font-bold text-gray-800">{{ props.title }}</h2>
+      <div class="text-gray-600 text-sm mt-2 flex space-x-4">
+        <span>{{ props.city }}</span>
+        <span>|</span>.
+        <span>{{ props.players }} Players</span>
+        <span>|</span>
+        <span>{{ props.time }} Hours</span>
+      </div>
+    </div>
+  </div>
+</template>
